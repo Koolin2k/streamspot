00872ceb747b67a00f9b3c905cde4dd2b0b2b5cf
Donte Dixon <dontedixon1@gmail.com>
2025-06-30 09:23:41 -0400

    Update next.config.js

--- project/next.config.js
+++ project/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  // Add other valid config options here if needed
+  experimental: {},
 };
 
 module.exports = nextConfig;
-
