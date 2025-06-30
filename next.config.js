7ee06e599633b45fb8176b04efe875979a577566
Donte Dixon <dontedixon1@gmail.com>
2025-06-30 03:49:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone', // changed from 'export' to allow dynamic rendering
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    domains: ['images.pexels.com'],
+  },
 };
 
 module.exports = nextConfig;
